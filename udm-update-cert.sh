344fd49ec64bd54eba5144eb6d31fce9bdf11473
Dan <danmalone326@gmail.com>
2023-01-16 13:43:01 -0800

    add path to ssh

--- udm-update-cert.sh
+++ udm-update-cert.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/bin/cat /etc/letsencrypt/live/router-internal.malone.org/fullchain.pem | /usr/bin/ssh udm-cert-update "cert"
-/bin/cat /etc/letsencrypt/live/router-internal.malone.org/privkey.pem | /usr/bin/ssh udm-cert-update "key"
-ssh udm-cert-update "restart"
+/bin/cat /etc/letsencrypt/live/router-internal.malone.org/fullchain.pem | /usr/bin/ssh udm-update-cert "cert"
+/bin/cat /etc/letsencrypt/live/router-internal.malone.org/privkey.pem | /usr/bin/ssh udm-update-cert "key"
+/usr/bin/ssh udm-update-cert "restart"
 
