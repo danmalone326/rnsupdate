c20aa957feb112c51c4bb5e942fd0f607a275efb
Dan <danmalone326@gmail.com>
2023-01-16 13:34:02 -0800

    typo

--- udm-update-cert.sh
+++ udm-update-cert.sh
@@ -2,5 +2,5 @@
 
 /bin/cat /etc/letsencrypt/live/router-internal.malone.org/fullchain.pem | /usr/bin/ssh udm-cert-update "cert"
 /bin/cat /etc/letsencrypt/live/router-internal.malone.org/privkey.pem | /usr/bin/ssh udm-cert-update "key"
-ssh udm-cert-update "restart
+ssh udm-cert-update "restart"
 
