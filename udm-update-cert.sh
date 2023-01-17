0836fde3853aa722eaa23fdbcd16c70bbb38c979
Dan <danmalone326@gmail.com>
2023-01-17 06:25:06 -0800

    comments

--- udm-update-cert.sh
+++ udm-update-cert.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
+#
+# add this to /etc/letsencrypt/renewal/<domain>.conf as
+# renew_hook = /path/to/udm-update-cert.sh
+#
 /bin/cat /etc/letsencrypt/live/router-internal.malone.org/fullchain.pem | /usr/bin/ssh udm-update-cert "cert"
 /bin/cat /etc/letsencrypt/live/router-internal.malone.org/privkey.pem | /usr/bin/ssh udm-update-cert "key"
 /usr/bin/ssh udm-update-cert "restart"
